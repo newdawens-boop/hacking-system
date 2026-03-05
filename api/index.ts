154ce6335d004db3144989635090e6266f1d9360
newdawens-boop <newdawens@gmail.com>
2026-03-05 03:32:27 -0500

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,8 @@
+import { IncomingMessage, ServerResponse } from "http";
 
+export default function handler(req: IncomingMessage, res: ServerResponse) {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+
+  res.end("Hello World from Vercel + TypeScript!");
+}
