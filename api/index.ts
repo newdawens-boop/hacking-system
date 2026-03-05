ddf4b00ddb839ccaa01c1e3e2d6e75edab25b3a2
newdawens-boop <newdawens@gmail.com>
2026-03-05 05:41:20 -0500

    Delete api directory

--- api/index.ts
+++
@@ -1,6 +0,0 @@
-// api/index.ts (or src/index.ts)
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).send('Hello World from Vercel + TypeScript!');
-}
